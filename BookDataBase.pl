6dd89986fb7f85c260eafe36face0c188a6c4111
Kamil Łuczak <gitlab@luczakweb.pl>
2016-05-17 08:31:31 +0200

    Add Book DataBase

---
+++ BookDataBase.pl
@@ -0,0 +1,55 @@
+% Author: Bartosz Modrzyñski
+% Date: 2016-04-25
+
+%Baza danych
+%author- id, imie, nazwisko
+%book - id, tytul, autor, rodzaj, indeks
+%type - id rodzaju, nazwa
+
+go:- dynamic(author/3), dynamic(book/5), dynamic(type/2), program.
+
+
+program :-  new(BDatabase, dialog('Menu')),
+            send(BDatabase, append, text('Baza Danych ksi¹¿ek')),
+            send(BDatabase, append, button('Dodawanie autora',message(@prolog,add_author)),below),
+            send(BDatabase, append, button('Dodawanie ksiazki',message(@prolog,add_book)),below),
+            send(BDatabase, append, button('Dodanie gatunku',message(@prolog,add_type)),below),
+            send(BDatabase, append, button('Usuniecie autora',message(@prolog,remove_author)),below),
+            send(BDatabase, append, button('Usuniecie ksiazki',message(@prolog,remove_book)),below),
+            send(BDatabase, append, button('Usuniecie gatunku', message(@prolog,remove_type)),below),
+            send(BDatabase, append, button(exit, message(BDatabase, destroy)), below),
+       send(BDatabase, open).
+
+add_author:-
+        new(Window,dialog('Dodawanie autora')),
+        send_list(Window,append,[ text('Wpisz imie, nazwisko oraz miasto'),
+                  new(Id_autora,text_item('Id autora')),
+                  new(Imie, text_item('Imie autora')),
+                  new(Nazwisko,text_item('Nazwisko autora')),
+                  button('Dodaj autora',and(message(@prolog,add_new_author,Id_autora?selection, Imie?selection, Nazwisko?selection),message(Window,destroy)))
+                                ]),
+                  send(Window,open).
+
+add_new_author:-assert(author(Id_autora,Imie,Nazwisko)).
+
+%book - id, tytul, autor, rodzaj, indeks
+
+add_book:-
+        new(Window,dialog('Dodawanie autora')),
+        send_list(Window,append,[ text('Wpisz imie, nazwisko oraz miasto'),
+                  new(Id_ksiazki,text_item('Id ksiazki')),
+                  new(Tytul, text_item('Tytul')),
+                  new(text('Autor:')),
+                  new(Id_autora,text_item('Id autora')),
+                  new(Imie, text_item('Imie autora')),
+                  new(Nazwisko,text_item('Nazwisko autora')),
+                  new(Typ, text_item('Rodzaj ksiazki')),
+                  new(Indeks, text_item('Indeks')),
+                  button('Dodaj autora',and(message(@prolog,add_new_author,Id_autora?selection, Imie?selection, Nazwisko?selection, Id_ksiazki?selection, Tytul?selection, Typ?selection, Indeks?selection),message(Window,destroy)))
+                                ]),
+                  send(Window,open).
+
+add_new_book:-assert(author(Id_autora,Imie,Nazwisko)),assert(book(Id_ksiazki,Tytul,Typ,Indeks)).
+
+
+
