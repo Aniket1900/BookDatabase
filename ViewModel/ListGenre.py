2196d435363cab72c1545bfb60d028a246550f47
Bartosz <liberate1792@gmail.com>
2016-06-12 17:28:34 +0200

    Creating and modify of ListGenre.py

---
+++ ViewModel/ListGenre.py
@@ -0,0 +1,21 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from Models.PrologObjects import Genre
+
+
+class ListGenreWindow(QWidget):
+    def __init__(self, parent=None):
+        super(ListGenreWindow, self).__init__(parent=parent)
+        self.ui = uic.loadUi('Views/listGenreUi.ui', self)
+        self.loadAuthors()
+
+    def loadAuthors(self):
+        model = QStandardItemModel(self.ui.genreListView)
+        genres = Genres()
+
+        for genre in genres:
+            item.setCheckable(True)
+            model.appendRow(item)
+
+        self.ui.genreListView.setModel(model)
